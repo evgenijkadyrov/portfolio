171484c45e9fcf9acf060a910b93997118b04360
Yauheni Kadyrau <7077634@gmail.com>
2022-06-20 12:02:36 +0300

    addd icons to tech stack

--- src/Skills/Skill/Skill.jsx
+++ src/Skills/Skill/Skill.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import style from './Skill.module.css'
+import style from './Skill.module.scss'
 
 const Skill = (props) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+            <div className={style.icon}>
+                <img src={props.icon}/>
+            </div>
             <h4>{props.title}</h4>
-            <span className={style.description}>{props.description}</span>
+           {/* <span className={style.description}>{props.description}</span>*/}
         </div>
     );
 };
