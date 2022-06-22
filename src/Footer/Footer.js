c78f6843bf1091d8c270e41dd73a0a3f46e995ce
Yauheni Kadyrau <7077634@gmail.com>
2022-06-22 22:20:54 +0300

    fix skills, add icon

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import style from './Footer.module.css'
+import style from './Footer.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+
+
 
 const Footer = () => {
     return (
         <div className={style.footer_wrapper}>
             <div className={`${styleContainer.container} ${style.container}`}>
                 <div className={style.items}>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
+
+                    <div className={style.icon}> <FontAwesomeIcon icon={faLinkedin} size={'3x'}/></div>
+
+                    <div className={style.icon}><FontAwesomeIcon icon={faGithubSquare} size={'3x'}/></div>
                     <div className={style.icon}></div>
                     <div className={style.icon}></div>
                 </div>
