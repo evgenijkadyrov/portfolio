cf1019a0ff5fb0fbf8d8f214e713f8021091582c
Yauheni Kadyrau <7077634@gmail.com>
2022-06-22 23:35:08 +0300

    change color contact, try fix footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,7 @@ import style from './Footer.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 
@@ -12,11 +12,15 @@ const Footer = () => {
         <div className={style.footer_wrapper}>
             <div className={`${styleContainer.container} ${style.container}`}>
                 <div className={style.items}>
+                    <div className={style.icon}>
+                        <FontAwesomeIcon className={style.icon } icon={faLinkedin} size={'3x'}/>
+                    </div>
+                    <div className={`${style.icon} ${style.round}`}>
+                        <FontAwesomeIcon icon={faGithub} size={'3x'}/>
+                    </div>
 
-                    <div className={style.icon}> <FontAwesomeIcon icon={faLinkedin} size={'3x'}/></div>
 
-                    <div className={style.icon}><FontAwesomeIcon icon={faGithubSquare} size={'3x'}/></div>
-                    <div className={style.icon}></div>
+
                     <div className={style.icon}></div>
                 </div>
             </div>
